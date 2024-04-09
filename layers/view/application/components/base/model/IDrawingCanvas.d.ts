8acd643cb86bed313b4cb25f4fe1b232b28fa1de
Emmanuel Paul <scansioquielom@gmail.com>
2024-04-09 19:44:53 +0100

    v0.1.36
    Signed-off-by: Emmanuel Paul <scansioquielom@gmail.com>

--- layers/view/application/components/base/model/IDrawingCanvas.d.ts
+++ layers/view/application/components/base/model/IDrawingCanvas.d.ts
@@ -1,6 +1,8 @@
 import IPosition from '../../../../../../common/models/IPosition';
 import BaseComponent from '../BaseComponent';
 interface IDrawingCanvas extends BaseComponent {
+    activateDesignMode(): void;
+    activatePreviewMode(): void;
     addDesignElement(element: HTMLElement, position?: IPosition): HTMLElement;
 }
 export default IDrawingCanvas;
--- layers/view/application/components/drawingcanvas/DrawingCanvas.d.ts
+++ layers/view/application/components/drawingcanvas/DrawingCanvas.d.ts
@@ -2,9 +2,18 @@ import IAnyObject from '../../../../../common/models/IAnyObject';
 import IPosition from '../../../../../common/models/IPosition';
 import BaseComponent from '../base/BaseComponent';
 import IDrawingCanvas from '../base/model/IDrawingCanvas';
+export declare enum DesignMode {
+    PREVIEWING = 0,
+    DESIGNING = 1
+}
 declare class DrawingCanvas extends BaseComponent implements IDrawingCanvas {
+    mode: DesignMode;
     constructor(style?: IAnyObject);
     onwheel: (event: WheelEvent) => void;
+    activateDesignMode(): void;
+    activatePreviewMode(): void;
+    private traverseAndActivateDesignMode;
+    private traverseAndActivatePreviewMode;
     addDesignElement(element: HTMLElement, position?: IPosition): HTMLElement;
 }
 export default DrawingCanvas;
